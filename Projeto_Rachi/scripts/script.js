f2646a96e3451ca71f4f4a75035c7bc16d6471a4
Anderson Maschio <80135294+yNinooo@users.noreply.github.com>
2024-03-22 21:48:37 -0300

    Add files via upload

---
+++ Projeto_Rachi/scripts/script.js
@@ -0,0 +1,7 @@
+/***********************************************************************************/
+const hamburger = document.querySelector(".header-style__links")
+
+document.getElementById("button_menu").addEventListener("click", () => {
+    hamburger.classList.toggle("open")
+})
+/***********************************************************************************/
